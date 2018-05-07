32cbc2217e320962b7484eadf38147b8eccb4ab5
Josh Coast <josh@joshcoast.com>
2018-05-06 22:33:41 -0600

    remove comments and debugging code

--- server.js
+++ server.js
@@ -1,16 +1,10 @@
-//Redoing the assignment by following along with the walk through.
-
 var bodyParser = require('body-parser');
 var express = require('express');
 var path = require('path');
 
 var app = express();
-// This port will take what ever is assigned by the 
-// deployment env (Heroku) and default to 8080 if 
-// not assigned (like your local).
 const PORT = process.env.PORT || 8080;
  
-// parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
  
 // parse application/json
